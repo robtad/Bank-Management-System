0946b31275f70586e3b5cbc612d6a8f8aa862ea3
massyakur <200201147@kocaeli.edu.tr>
2022-05-13 01:59:21 +0300

    bankaTarihi updated

--- BankMS/managerChildForm/bankaTarihiForm.cs
+++ BankMS/managerChildForm/bankaTarihiForm.cs
@@ -7,23 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SLRDbConnector;
 
 namespace BankMS.managerChildForm
 {
     public partial class bankaTarihiForm : Form
     {
+        DbConnector db = new DbConnector();
+        DateTime bankDate;
+
         public bankaTarihiForm()
         {
             InitializeComponent();
         }
-
         private void bankaTarihiForm_Load(object sender, EventArgs e)
         {
-            //Change with date in database
-            labelDate.Text = DateTime.Now.ToLongDateString();
+            string dateTemp;
+            db.getSingleValue("Select * From Date", out dateTemp, 0);
+            bankDate = DateTime.Parse(dateTemp);
+            labelDate.Text = bankDate.ToLongDateString();
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            bankDate = bankDate.AddMonths(1);
+            labelDate.Text = bankDate.ToLongDateString();
+            db.performCRUD("UPDATE Date SET BankDate = '"+bankDate+"'");
+            
+        }
+
+        private void paySalary()
         {
 
         }
