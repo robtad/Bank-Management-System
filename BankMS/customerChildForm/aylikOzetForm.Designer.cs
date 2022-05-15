04381c12fa436c3a68f1877fc46529f62c00a7df
massyakur <200201147@kocaeli.edu.tr>
2022-05-15 22:55:31 +0300

    customerAylikOzeti

--- BankMS/customerChildForm/aylikOzetForm.Designer.cs
+++ BankMS/customerChildForm/aylikOzetForm.Designer.cs
@@ -29,6 +29,25 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewParaGonder = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewBorc = new System.Windows.Forms.DataGridView();
+            this.dataGridViewYatir = new System.Windows.Forms.DataGridView();
+            this.dataGridViewCek = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParaGonder)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBorc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewYatir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCek)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -36,22 +55,136 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(232, 179);
+            this.label1.Location = new System.Drawing.Point(109, -113);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(291, 25);
+            this.label1.Size = new System.Drawing.Size(150, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "AYLIK OZET GORUNTULE";
+            this.label1.Text = "AYLIK ÖZETİ";
+            this.label1.UseMnemonic = false;
             // 
-            // aylikOzet
+            // dataGridViewParaGonder
+            // 
+            this.dataGridViewParaGonder.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewParaGonder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParaGonder.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewParaGonder.Name = "dataGridViewParaGonder";
+            this.dataGridViewParaGonder.Size = new System.Drawing.Size(744, 301);
+            this.dataGridViewParaGonder.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 71);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(761, 336);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridViewParaGonder);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(753, 310);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Para Gönderim";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridViewCek);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(753, 310);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Para Çekme";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridViewYatir);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(753, 310);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Para Yatırma";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dataGridViewBorc);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(753, 310);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Borç Ödeme";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "AYLIK ÖZETİ";
+            // 
+            // dataGridViewBorc
+            // 
+            this.dataGridViewBorc.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewBorc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBorc.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewBorc.Name = "dataGridViewBorc";
+            this.dataGridViewBorc.Size = new System.Drawing.Size(744, 301);
+            this.dataGridViewBorc.TabIndex = 6;
+            // 
+            // dataGridViewYatir
+            // 
+            this.dataGridViewYatir.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewYatir.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewYatir.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewYatir.Name = "dataGridViewYatir";
+            this.dataGridViewYatir.Size = new System.Drawing.Size(744, 301);
+            this.dataGridViewYatir.TabIndex = 7;
+            // 
+            // dataGridViewCek
+            // 
+            this.dataGridViewCek.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewCek.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCek.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewCek.Name = "dataGridViewCek";
+            this.dataGridViewCek.Size = new System.Drawing.Size(744, 301);
+            this.dataGridViewCek.TabIndex = 8;
+            // 
+            // aylikOzetForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(751, 439);
+            this.ClientSize = new System.Drawing.Size(951, 520);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "aylikOzet";
+            this.Name = "aylikOzetForm";
             this.Text = "aylikOzet";
+            this.Load += new System.EventHandler(this.aylikOzetForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParaGonder)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBorc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewYatir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCek)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,5 +193,15 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewParaGonder;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewBorc;
+        private System.Windows.Forms.DataGridView dataGridViewYatir;
+        private System.Windows.Forms.DataGridView dataGridViewCek;
     }
 }
