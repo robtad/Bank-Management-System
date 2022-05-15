e7a122593d2f10b7899f71f452120d3f5185f9dd
massyakur <200201147@kocaeli.edu.tr>
2022-05-15 23:08:05 +0300

    before send

--- BankMS/tellerChildForm/musteriIslemlerForm.Designer.cs
+++ BankMS/tellerChildForm/musteriIslemlerForm.Designer.cs
@@ -28,20 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridViewParaGonder = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridViewCek = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridViewYatir = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dataGridViewBorc = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParaGonder)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCek)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewYatir)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBorc)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "MÜŞTERİ İŞLEMLER";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 67);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(761, 336);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridViewParaGonder);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(753, 310);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Para Gönderim";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewParaGonder
+            // 
+            this.dataGridViewParaGonder.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewParaGonder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParaGonder.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewParaGonder.Name = "dataGridViewParaGonder";
+            this.dataGridViewParaGonder.Size = new System.Drawing.Size(744, 301);
+            this.dataGridViewParaGonder.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridViewCek);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(753, 310);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Para Çekme";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewCek
+            // 
+            this.dataGridViewCek.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewCek.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCek.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewCek.Name = "dataGridViewCek";
+            this.dataGridViewCek.Size = new System.Drawing.Size(744, 301);
+            this.dataGridViewCek.TabIndex = 8;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridViewYatir);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(753, 310);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Para Yatırma";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewYatir
+            // 
+            this.dataGridViewYatir.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewYatir.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewYatir.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewYatir.Name = "dataGridViewYatir";
+            this.dataGridViewYatir.Size = new System.Drawing.Size(744, 301);
+            this.dataGridViewYatir.TabIndex = 7;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dataGridViewBorc);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(753, 310);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Borç Ödeme";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewBorc
+            // 
+            this.dataGridViewBorc.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewBorc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBorc.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewBorc.Name = "dataGridViewBorc";
+            this.dataGridViewBorc.Size = new System.Drawing.Size(744, 301);
+            this.dataGridViewBorc.TabIndex = 6;
             // 
             // musteriIslemlerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(858, 539);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "musteriIslemlerForm";
             this.Text = "musteriIslemlerForm";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParaGonder)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCek)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewYatir)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBorc)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridViewParaGonder;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridViewCek;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridViewYatir;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataGridView dataGridViewBorc;
     }
 }
--- BankMS/tellerChildForm/musteriIslemlerForm.cs
+++ BankMS/tellerChildForm/musteriIslemlerForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SLRDbConnector;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,45 @@ namespace BankMS.tellerChildForm
         public musteriIslemlerForm()
         {
             InitializeComponent();
-        }
-    }
+			showTransactions();
+
+		}
+		DbConnector db = new DbConnector();
+
+		private void showTransactions()
+		{
+			paraGonder();
+			borcOdeme();
+			paraYatir();
+			paraCek();
+		}
+
+		private void paraGonder()
+		{
+			db.fillDataGridView(@"SELECT t.id, t.AccountNo AS Kaynak, tr.TargetAccountNo AS Hedef, t.Balance AS Bakiye, p.Amount AS Tutar, Currency, t.Date
+								FROM TransactionTbl t, Payment p, TransferTbl tr
+								WHERE t.id = p.TransactionID AND tr.PaymentID = p.id", dataGridViewParaGonder);
+		}
+
+		private void borcOdeme()
+		{
+			db.fillDataGridView(@"SELECT t.id, t.AccountNo, p.Amount, t.Balance, Date
+								FROM TransactionTbl t, Payment p, LoanRepayment l
+								WHERE t.id = p.TransactionID AND l.PaymentID = p.id", dataGridViewBorc);
+		}
+
+		private void paraYatir()
+		{
+			db.fillDataGridView(@"SELECT t.id, t.AccountNo, p.Amount, t.Balance, Date
+								FROM TransactionTbl t, Payment p, Deposit d
+								WHERE t.id = p.TransactionID AND d.PaymentID = p.id", dataGridViewYatir);
+		}
+
+		private void paraCek()
+		{
+			db.fillDataGridView(@"SELECT t.id, t.AccountNo, p.Amount, t.Balance, Date
+								FROM TransactionTbl t, Payment p, Withdrawal w
+								WHERE t.id = p.TransactionID AND w.PaymentID = p.id", dataGridViewCek);
+		}
+	}
 }
